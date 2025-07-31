a3fce1b9f44a04c59ab381d0a67d97eff9fab0f3
dilip1996-git <dilip1996-git@users.noreply.github.com>
2025-07-31 21:25:28 +0530

    Updated ststic folder name

--- Techblume_Funding/settings.py
+++ Techblume_Funding/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # For development
+STATIC_URL = '/Static/'
+STATICFILES_DIRS = [BASE_DIR / 'Static']  # For development
 STATIC_ROOT = os.path.join( BASE_DIR / 'staticfiles')    # For collectstatic in production
 
 
